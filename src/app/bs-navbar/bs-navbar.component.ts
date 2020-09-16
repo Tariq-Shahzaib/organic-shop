c893cbf80a9b8d1f3b66712fa1a857acb17fc407
Shahzaib Tariq <Shahzaib.tariq@powercoders.org>
2020-09-16 15:52:32 +0200

    Navbar with authentification

---
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
+@Component({
+  selector: 'bs-navbar',
+  templateUrl: './bs-navbar.component.html',
+  styleUrls: ['./bs-navbar.component.css'],
+})
+export class BsNavbarComponent {
+  user$: Observable<firebase.User>;
+
+  constructor(public auth: AuthService) {}
+
+  logout() {
+    this.auth.logout();
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAMPsNwYlpkMJsqZFMSyEDeXOqTYd6G4ZQ',
+    authDomain: 'oshop-9f91a.firebaseapp.com',
+    databaseURL: 'https://oshop-9f91a.firebaseio.com',
+    projectId: 'oshop-9f91a',
+    storageBucket: 'oshop-9f91a.appspot.com',
+    messagingSenderId: '43302725261',
+    appId: '1:43302725261:web:3f193649ebf301d9a264c4',
+    measurementId: 'G-KRMS3C945X',
+  },
 };
