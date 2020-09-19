37966ad4e1e4198e1509c3e802a4d0253e668deb
Shahzaib Tariq <shahzaibzaibi001@gmail.com>
2020-09-19 15:10:02 +0200

    Applied filter method

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 import { ProductService } from 'src/app/product.service';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.css'],
+  styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: any[];
+  filtredProducts: any[];
+  subscrition: Subscription;
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getall();
+    this.subscrition = this.productService
+      .getall()
+      .subscribe(
+        products =>
+          (this.filtredProducts = this.products = products.map(a =>
+            a.payload.val()
+          ))
+      );
+  }
+  ngOnDestroy() {
+    this.subscrition.unsubscribe();
+  }
+  filter(query: String) {
+    this.filtredProducts = query
+      ? this.products.filter(p =>
+          p.title.toLowerCase().includes(query.toLowerCase())
+        )
+      : this.products;
   }
 
   ngOnInit(): void {}
